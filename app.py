2961b427fa86c7a1f73f2007f0a2c35e5a91a7c7
ivan <juniorivandias@gmail.com>
2024-09-15 10:06:36 -0300

    load

--- app.py
+++ app.py
@@ -2,31 +2,44 @@ import streamlit as st
 import requests
 import extra_streamlit_components as stx
 
-# Título da aplicação
 st.title("Ofertas personalizadas por cliente")
 
-# Campo de input
+if 'nome' not in st.session_state:
+    st.session_state.nome = ""
+
+if 'data' not in st.session_state:
+    st.session_state.data = []
+
+if 'mensagem' not in st.session_state:
+    st.session_state.mensagem = []
+
 nome = st.text_input("Por favor, insira o código do cliente para prosseguir.")
 
+try:
+    if st.session_state.nome != nome:
+        print('teste1')
+        st.session_state.nome = nome 
 
+        data = []
+        mensagem = []
 
-# Verifica se o usuário inseriu algo
-if nome:
-    url = f'https://recomendacao-produtos.onrender.com/recomendar/produtos/{nome}'
-    response = requests.get(url)
-    if response.status_code == 200:
-        dados = response.json()
+        with st.spinner('Carregando recomendações...'):
+            
+                url = f'https://recomendacao-produtos.onrender.com/recomendar/produtos/{nome}'
+                response = requests.get(url)
+                if response.status_code == 200:
+                    dados = response.json()
+            
 
-    data = []
-    mensagem = []
-    for index, nome in enumerate(dados['produtos']):
-        data.append(stx.TabBarItemData(id=index, title=f'Oferta {index+1}', description=nome))
-        mensagem.append(dados['oferta'][index]['mensagem'])
+        for index, nome in enumerate(dados['produtos']):
+            data.append(stx.TabBarItemData(id=index, title=f'Oferta {index+1}', description=nome))
+            mensagem.append(dados['oferta'][index]['mensagem'])
 
-    print(mensagem)
-
-    index_selecionado = stx.tab_bar(data=data, default=0)
-    print(index_selecionado)
-    st.info(mensagem[int(index_selecionado)])
-
-    
+        st.session_state.data = data
+        st.session_state.mensagem = mensagem
+        
+    if len(st.session_state.mensagem) > 0:
+        index_selecionado = stx.tab_bar(data=st.session_state.data, default=0)
+        st.info(st.session_state.mensagem[int(index_selecionado)])
+except Exception as e:
+            st.error(f"Ocorreu um erro ao buscar as recomendações.")
